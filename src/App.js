a13b1f23a75ef08c4840eb0615c7177700f75474
Sabina Schmitz <sabinahschmitz@gmail.com>
2022-06-21 21:39:21 +0200

    added background, searchbar, dates and some styling

--- src/App.js
+++ src/App.js
@@ -5,14 +5,46 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month =months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
-    <div className="App">
-      test test test
+    <div className="app warm">
+      <main>
+        <div className="search-box">
+
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
+        </div>
+
+        <div className="location-box">
+          <div className="location"> Düsseldorf, Germany</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+      </div>
+
+      <div className="weather-box">
+        <div className="temp">
+          15°C
+        </div>
+        <div className="weather">
+          Sunny
+        </div>
+      </div>
+      </main>
     </div>
   );
 }
 
 export default App;
-
-//Tutorial: https://www.youtube.com/watch?v=GuA0_Z1llYU
-//npm start after cding to the folder 5:14
