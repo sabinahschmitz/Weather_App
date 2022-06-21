e8eee7d2ab4fab2c3827612e3f44fe28d1c369a1
Sabina Schmitz <sabinahschmitz@gmail.com>
2022-06-21 13:34:04 +0200

    basic files for weather app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+//api key saved in an object
+const api = {
+  key: "896bcef6e113845505149836504d38ea",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
+
+//Tutorial: https://www.youtube.com/watch?v=GuA0_Z1llYU
+//npm start after cding to the folder 5:14
