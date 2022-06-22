06feb531830effdc0fdd8f12d3cad344be7f4ff4
Sabina Schmitz <sabinahschmitz@gmail.com>
2022-06-22 13:31:41 +0200

    Complete

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+
+import { toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers';
+import React, {useState} from 'react';
 //api key saved in an object
 const api = {
   key: "896bcef6e113845505149836504d38ea",
@@ -6,6 +9,23 @@ const api = {
 
 function App() {
 
+  // get cached data
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+
+  //function used to get weather, fetch request to get it from the apu base
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result =>{
+          setQuery('');
+           setWeather(result);
+        });
+    }
+  }
+
+  // Creates the date function
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -15,10 +35,16 @@ function App() {
     let month =months[d.getMonth()];
     let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
+    return `${day}, ${date}. ${month} ${year}`
   }
   return (
-    <div className="app warm">
+    // so if we havent done a search query, just make it app, if not, check if temp is over 16 degrees, if yes, then return warm.
+    <div className={
+      (typeof weather.main != "undefined") 
+       ? (weather.main.temp > 16 
+        ? 'app warm' 
+        : 'app')
+      : 'app'}>
       <main>
         <div className="search-box">
 
@@ -26,22 +52,23 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
-
-        <div className="location-box">
-          <div className="location"> Düsseldorf, Germany</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-      </div>
-
-      <div className="weather-box">
-        <div className="temp">
-          15°C
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location"> {weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+              <div className="temp">{Math.round (weather.main.temp)}°C</div>
+              <div className="weather">{weather.weather[0].main} </div>
+         </div>
         </div>
-        <div className="weather">
-          Sunny
-        </div>
-      </div>
+      ):('')}
       </main>
     </div>
   );
